cd96af4db5d5067c38d7dc154e7821957a852123
Kautsar A B <kautsar.ab@gmail.com>
2022-12-01 16:15:23 +0000

    phase 2

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,60 @@
 use std::{
+    collections::BTreeMap,
     fs::File,
     io::{BufRead, BufReader},
 };
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct ElveRank {
+    total_calories: i64,
+    total_snack: i64,
+}
 
 fn main() {
     let file = File::open("./input-1").unwrap();
     let reader = BufReader::new(file);
 
-    let mut ln = 0;
+    let mut elve_list: BTreeMap<ElveRank, i32> = BTreeMap::new();
 
-    let mut chosen_elve = -1_i64;
-    let mut max_cal_sum = 0_i64;
+    {
+        let mut elv_n = 0_i32;
+        let mut sn_cnt = 0_i64;
+        let mut cal_sum = 0_i64;
 
-    let mut elv_n = 0_i64;
-    let mut cal_sum = 0_i64;
+        for line in reader.lines() {
+            let l = line.unwrap();
 
-    println!("elve#{elv_n}");
-    for line in reader.lines() {
-        let l = line.unwrap();
+            if l == "" {
+                elv_n += 1;
 
-        if l == "" {
-            ln = 0;
-            elv_n += 1;
+                elve_list.insert(
+                    ElveRank {
+                        total_calories: cal_sum,
+                        total_snack: sn_cnt,
+                    },
+                    elv_n,
+                );
 
-            println!("elve={elv_n}\tcal={cal_sum}");
+                sn_cnt = 0;
+                cal_sum = 0;
+                continue;
+            }
 
-            choose_max(&mut chosen_elve, &mut max_cal_sum, elv_n, cal_sum);
+            let cal = l.parse::<i64>().unwrap();
 
-            cal_sum = 0;
-            continue;
+            cal_sum += cal;
+            sn_cnt += 1;
         }
-
-        let cal = l.parse::<i64>().unwrap();
-        ln += 1;
-
-        cal_sum += cal;
     }
 
-    println!("=======");
-    println!("final_elve={chosen_elve}\tfinal_cal={max_cal_sum}")
-}
+    println!("=============");
 
-fn choose_max(me: &mut i64, msum: &mut i64, ce: i64, csum: i64) {
-    if *msum < csum {
-        *me = ce;
-        *msum = csum;
+    let mut top_sum = 0;
+    let mut iter = elve_list.iter().rev();
+    for i in 1..4 {
+        let el = iter.next().unwrap();
+        top_sum += el.0.total_calories;
+        println!("#{i} elv={} total={:?}", el.1, el.0)
     }
+    println!("total={top_sum}")
 }
